12168b394b7b4feab3a76845db914688bd6ea80b
Gabriel Mawule Sakyi <gabriel.sakyi@amalitech.com>
2024-11-06 17:20:59 +0000

    Initial commit: Implemented methods to input matrix sizes and elements from the user.

---
+++ src/Matrixmultiplication.java
@@ -0,0 +1,66 @@
+import java.util.Scanner;
+
+public class Matrixmultiplication {
+
+    int[][] matrixA, matrixB, matrixC;
+    int matrixA_row, matrixA_col, matrixB_row, matrixB_col;
+    Scanner input;
+
+
+    public Matrixmultiplication(){
+        input = new Scanner(System.in);
+
+        System.out.print("Enter the number of rows and columns for Matrix A: ");
+        matrixA_row = input.nextInt();
+        matrixA_col = input.nextInt();
+
+        System.out.print("Enter the number of rows and columns for Matrix B: ");
+        matrixB_row = input.nextInt();
+        matrixB_col = input.nextInt();
+
+        if(matrixA_col != matrixB_row){
+            System.out.println("This operation won't be possible because the row of Matrix A should be equal to the column of Matrix B");
+        }
+        else{
+            matrixOperation();
+        }
+
+    }
+
+    private void matrixOperation() {
+        matrixA = new int[matrixA_row][matrixA_col];
+        matrixB = new int[matrixB_row][matrixB_col];
+        matrixC = new int[matrixA_row][matrixB_col];
+
+        System.out.println("Enter " + matrixA_row * matrixA_col + " digits for matrixA" );
+        for(int i=0; i<matrixA_row; i++){
+            for(int j=0; j<matrixA_col; j++){
+                matrixA[i][j] = input.nextInt();
+            }
+        }
+
+        System.out.println("Enter " + matrixB_row * matrixB_col + " digits for matrixB" );
+        for(int i=0; i<matrixB_row; i++){
+            for(int j=0; j<matrixB_col; j++){
+                matrixB[i][j] = input.nextInt();
+            }
+        }
+
+        for(int i=0; i<matrixA.length; i++){
+            for(int j=0; j<matrixB[0].length; j++){
+
+
+            }
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+
+        Matrixmultiplication matrixmultiplication = new Matrixmultiplication();
+
+
+    }
+
+}
