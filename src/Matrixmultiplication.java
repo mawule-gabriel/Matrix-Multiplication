b35897189da6b377f44039efb061ff4b6df43fa7
Mawule Gabriel Sakyi <gabriel.sakyi@amalitech.com>
2024-11-08 11:34:15 +0000

    Made changes for the user to know which index they are inputing an element

--- src/Matrixmultiplication.java
+++ src/Matrixmultiplication.java
@@ -10,21 +10,31 @@ public class Matrixmultiplication {
     public Matrixmultiplication(){
         input = new Scanner(System.in);
 
-        System.out.print("Enter the number of rows and columns for Matrix A: ");
+        // Getting dimensions for Matrix A
+        System.out.print("Enter the number of rows and columns for Matrix A (rows columns): ");
+        while(!input.hasNextInt()){
+            System.out.println("Please enter a valid value for rows and columns (1-100): ");
+            input.next();
+        }
         matrixA_row = input.nextInt();
         matrixA_col = input.nextInt();
 
-        System.out.print("Enter the number of rows and columns for Matrix B: ");
+        // Getting dimensions for Matrix B
+        System.out.print("Enter the number of rows and columns for Matrix B (rows columns): ");
+        while(!input.hasNextInt()){
+            System.out.println("Please enter a valid value for rows and columns (1-100): ");
+            input.next();
+        }
         matrixB_row = input.nextInt();
         matrixB_col = input.nextInt();
 
+        // Checking matrix multiplication condition
         if(matrixA_col != matrixB_row){
-            System.out.println("This operation won't be possible because the row of Matrix A should be equal to the column of Matrix B");
+            System.out.println("Matrix multiplication is not possible because the number of columns in Matrix A should be equal to the number of rows in Matrix B.");
         }
         else{
             matrixOperation();
         }
-
     }
 
     private void matrixOperation() {
@@ -32,52 +42,56 @@ public class Matrixmultiplication {
         matrixB = new int[matrixB_row][matrixB_col];
         matrixC = new int[matrixA_row][matrixB_col];
 
-        System.out.println("Enter " + matrixA_row * matrixA_col + " digits for matrixA" );
-        for(int i=0; i<matrixA_row; i++){
-            for(int j=0; j<matrixA_col; j++){
+        // Input for Matrix A
+        System.out.println("Enter " + matrixA_row * matrixA_col + " elements for Matrix A:");
+        for(int i = 0; i < matrixA_row; i++){
+            for(int j = 0; j < matrixA_col; j++){
+                // Prompting user with index for each element in matrix A
+                System.out.print("Enter element [" + i + "][" + j + "] for Matrix A: ");
                 while(!input.hasNextInt()){
                     System.out.println("Invalid input. Please enter an integer: ");
-                    input.next();
+                    input.next(); // discard invalid input
                 }
                 matrixA[i][j] = input.nextInt();
             }
         }
 
-        System.out.println("Enter " + matrixB_row * matrixB_col + " digits for matrixB" );
-        for(int i=0; i<matrixB_row; i++){
-            for(int j=0; j<matrixB_col; j++){
+        // Input for Matrix B
+        System.out.println("Enter " + matrixB_row * matrixB_col + " elements for Matrix B:");
+        for(int i = 0; i < matrixB_row; i++){
+            for(int j = 0; j < matrixB_col; j++){
+                // Prompting user with index for each element in matrix B
+                System.out.print("Enter element [" + i + "][" + j + "] for Matrix B: ");
                 while(!input.hasNextInt()){
                     System.out.println("Invalid input. Please enter an integer: ");
+                    input.next(); // discard invalid input
                 }
                 matrixB[i][j] = input.nextInt();
             }
         }
 
-        for(int i=0; i<matrixA.length; i++){
-            for(int j=0; j<matrixB[0].length; j++){
+        // Matrix multiplication logic
+        for(int i = 0; i < matrixA.length; i++){
+            for(int j = 0; j < matrixB[0].length; j++){
                 matrixC[i][j] = 0;
-
-                for(int k=0; k<matrixA[0].length; k++){
-                    matrixC[i][j] = matrixC[i][j] + matrixA[i][j] * matrixB[k][j];
+                for(int k = 0; k < matrixA[0].length; k++){
+                    matrixC[i][j] += matrixA[i][k] * matrixB[k][j];
                 }
             }
         }
 
-        for(int[] a : matrixC){
-            for(int elements : a){
-                System.out.print(elements + " ");
+        // Output the resulting matrix C
+        System.out.println("The results for matrix C is: ");
+        for(int[] row : matrixC){
+            for(int element : row){
+                System.out.print(element + " ");
             }
             System.out.println();
         }
-
-
     }
 
     public static void main(String[] args) {
 
-        Matrixmultiplication matrixmultiplication = new Matrixmultiplication();
-
-
+        new Matrixmultiplication();
     }
-
 }
